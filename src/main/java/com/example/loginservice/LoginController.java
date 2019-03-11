1c2ea9537a61b13ae151c479a800a54cb91cf2e7
fathergeorge <enclaveunit@gmail.com>
2019-03-11 14:54:50 -0400

    LoginController : login() returns not found

--- src/main/java/com/example/loginservice/LoginController.java
+++ src/main/java/com/example/loginservice/LoginController.java
@@ -1,4 +1,24 @@
 package com.example.loginservice;
 
-public class LoginController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+final class LoginController {
+
+    Map<String, LoginInfo> stubRepository = new HashMap<>();
+    {
+        stubRepository.put("Euler", new LoginInfo("Euler", "2.27"));
+    }
+
+    @PostMapping("/login")
+    LoginResponse login(@RequestBody Optional<LoginInfo> request) {
+
+        return new LoginResponse("-1", "Account not found");
+    }
 }
--- src/test/java/com/example/loginservice/LoginControllerTest.java
+++ src/test/java/com/example/loginservice/LoginControllerTest.java
@@ -1,19 +1,51 @@
 package com.example.loginservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginControllerTest {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    public void loginReturnsNotFoundIfProfessorNotInDb() throws Exception {
+        // Setup
+        final LoginInfo request = new LoginInfo("Euler", "2.72");
 
+        // Execute
+        final String actualStringResponse = mockMvc.perform(post("/login")
+                                                   .contentType(MediaType.APPLICATION_JSON)
+                                                   .content(OBJECT_MAPPER.writeValueAsString(request)))
+                                                   .andExpect(status().isOk())
+                                                   .andReturn()
+                                                   .getResponse()
+                                                   .getContentAsString();
+        LoginResponse actualResponse = OBJECT_MAPPER.readValue(actualStringResponse, LoginResponse.class);
+
+        // Assert
+        assertThat(actualResponse, allOf(hasProperty("statusCode", is("-1")),
+                                         hasProperty("statusDescription", is("Account not found"))
+        ));
+    }
 }
